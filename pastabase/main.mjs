830f770d4f87fa4db523a6d4b7e4f264f25015ed
Diego Oliveira <diego-o-a@hotmail.com>
2022-05-11 18:48:26 -0300

    Javascript

--- pastabase/main.mjs
+++
@@ -1,5 +0,0 @@
-import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
-
-console.log(mostraIdade('Diego', 26));
-console.log(mostraCidade('Diego', 'Florianopolis'));
-console.log(mostraHobby('Diego', 'Correr'));
